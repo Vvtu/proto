a5b24e5a742c667468b080473b87996d7bba0c54
Vvtu <vvtu@icloud.com>
2020-06-12 07:14:09 +0300

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8080;
-
+console.log("Hello world!");
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
 app.use(favicon(__dirname + "/build/favicon.png"));
